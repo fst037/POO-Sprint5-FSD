d6ccf4f62aae5d482d15bd6ad2d7533b3a1b4e14
BoFo <brunofazio466@gmail.com>
2023-10-23 20:46:03 -0300

    metodos basicos tarjetas

--- tarjetas.py
+++ tarjetas.py
@@ -1,8 +1,17 @@
 class Tarjeta:
+    def __init__(self, numero, fecha_vencimiento):
+        self.numero = numero
+        self.fecha_vencimiento = fecha_vencimiento
+
   
 
 class TarjetaCredito(Tarjeta):
-
+    def __init__(self, numero, fecha_vencimiento, limite_credito):
+        super().__init__(numero, fecha_vencimiento)
+        self.limite_credito = limite_credito
 
 class TarjetaDebito(Tarjeta):
+    def __init__(self, numero, fecha_vencimiento, saldo_disponible):
+        super().__init__(numero, fecha_vencimiento)
+        self.saldo_disponible = saldo_disponible
 
